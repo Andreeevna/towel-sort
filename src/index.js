530c50f208e094aa5b87856086aeece0cb1c0774
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2021-02-14 00:16:08 +0300

    feat: new type

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ module.exports = function towelSort (matrix) {
   return [];
   }
   for (let i = 0; i< matrix.length; i++){
-    if (i % 2 != 0) {
+    if (i % 2 !== 0) {
       matrix[i].reverse();
     } 
   }
